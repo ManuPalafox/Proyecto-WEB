b48a9d1dd870596d21ee195f6200bc8ffa2448d3
JMV76489 <95047311+JMV76489@users.noreply.github.com>
2023-01-04 20:11:31 -0600

    Agregado campo cuando se ingresa "otros" en E.F
    Agregado campo cuando se ingresa "otros" en Entidad Federativa

--- JS/form-validation.js
+++ JS/form-validation.js
@@ -1,19 +1,30 @@
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(() => {
-  'use strict'
+/*Funcion para mostrar campo de "otros" en la opcion de escuela de procedencia*/
+function mostrarCampo(name){
+  if(name=='otro')document.getElementById('otrocampo').style.display="block";
+  else document.getElementById('otrocampo').style.display = "none";
+}
 
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  const forms = document.querySelectorAll('.needs-validation')
+/*Funcion para ocultar campo de "otros" en la opcion de escuela de procedencia*/
+function ocultarCampo(){
+  document.getElementById('otrocampo').style.display = 'none';
+}
 
-  // Loop over them and prevent submission
-  Array.from(forms).forEach(form => {
-    form.addEventListener('submit', event => {
-      if (!form.checkValidity()) {
-        event.preventDefault()
-        event.stopPropagation()
-      }
+// // Example starter JavaScript for disabling form submissions if there are invalid fields
+// (() => {
+//   'use strict'
 
-      form.classList.add('was-validated')
-    }, false)
-  })
-})()
+//   // Fetch all the forms we want to apply custom Bootstrap validation styles to
+//   const forms = document.querySelectorAll('.needs-validation')
+
+//   // Loop over them and prevent submission
+//   Array.from(forms).forEach(form => {
+//     form.addEventListener('submit', event => {
+//       if (!form.checkValidity()) {
+//         event.preventDefault()
+//         event.stopPropagation()
+//       }
+
+//       form.classList.add('was-validated')
+//     }, false)
+//   })
+// })()
